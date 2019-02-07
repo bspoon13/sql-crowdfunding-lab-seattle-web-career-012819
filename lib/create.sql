e8049a4ff1ffd640408329f01f2daddb2f04ef48
github username <github email address>
2019-02-06 16:10:04 -0800

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,33 @@
+-- A project has a title, a category, a funding goal, a start date, and an end date.
+-- A user has a name and an age
+-- A pledge has an amount. It belongs to a user, and it also belongs to a project.
+--
+-- CREATE TABLE cats (
+--     id INTEGER PRIMARY KEY,
+--     name TEXT,
+--     age INTEGER,
+--     breed TEXT,
+--     owner_id INTEGER
+-- );
+
+CREATE TABLE projects (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date TEXT,
+  end_date TEXT
+);
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE pledges (
+  id INTEGER PRIMARY KEY,
+  amount INTEGER,
+  user_id INTEGER,
+  project_id INTEGER
+);
